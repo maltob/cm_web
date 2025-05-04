6415a0858290502d669e7e0a9e235be1f35dafdf
maltob <425906+maltob@users.noreply.github.com>
2025-05-04 15:00:41 -0400

    Add filtering to Software Update deployment table.

--- cm_web/src/components/reporting/deployments/SUMDeploymentDetails.vue
+++ cm_web/src/components/reporting/deployments/SUMDeploymentDetails.vue
@@ -3,6 +3,12 @@ import { CMRestService } from '@/service/CMRestService';
 import { onMounted, ref } from 'vue';
 import { DataTable, Column } from 'primevue';
 
+import Tag from 'primevue/tag';
+import InputText from 'primevue/inputtext';
+import Select from 'primevue/select';
+
+import { FilterMatchMode } from '@primevue/core/api';
+
 const props = defineProps({
     assignmentID: Number,
     statusType: Number
@@ -26,9 +32,19 @@ onMounted(() => {
 
 
 });
+
+
+const filters = ref({
+    StatusType: { value: null, matchMode: FilterMatchMode.EQUALS },
+    DeviceName: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    StatusDescription: { value: null, matchMode: FilterMatchMode.CONTAINS }
+});
+
+
 const appStatusTypeLookup = ["All","Success 🟩","In Progress 🟨","Requirements Not Met 🟦","Unknown ❔","Error 🟥"]
 const deploymentDetails = ref();
 const appStatusTypeStr = ref();
+const statuses = ref([1,2,3,4,5]);
 </script>
 
 <template>
@@ -36,13 +52,38 @@ const appStatusTypeStr = ref();
     
     <h1 class="text-xl font-bold pt-4">Software Update Deployment Details for <span class="font-extrabold">{{ deploymentDetails[0].AssignmentName }}</span> to <router-link class="underline" :to="{ name: 'devicecollection', params: { collectionID: deploymentDetails[0].CollectionID } }">{{ deploymentDetails[0].CollectionName }}</router-link><span v-if="props.statusType"> with a status of {{ appStatusTypeStr }}</span></h1>
 
-    <DataTable :value="deploymentDetails" stripedRows tableStyle="min-width: 50rem">
-        <Column field="DeviceName" header="DeviceName"></Column>
-        <Column field="StatusDescription" header="StatusDescription"></Column>
-        <Column field="StatusType" header="Status">
-       <template #body="slotProps">
-        {{appStatusTypeLookup[slotProps.data.StatusType]}}
-       </template> 
+    <DataTable :value="deploymentDetails" stripedRows tableStyle="min-width: 50rem"  v-model:filters="filters" filterDisplay="row">
+        <Column field="DeviceName" header="DeviceName">
+            <template #filter="{ filterModel, filterCallback }">
+                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()" placeholder="Search by device name" />
+                </template>
+            </Column>
+        <Column field="StatusDescription" header="StatusDescription">
+            <template #filter="{ filterModel, filterCallback }">
+                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()" placeholder="Search by status text" />
+                </template>
+        </Column>
+        <Column field="StatusType" header="Status" :showFilterMenu="false">
+        <template #body="slotProps">
+            {{appStatusTypeLookup[slotProps.data.StatusType]}}
+        </template>
+        <template #filter="{ filterModel, filterCallback }">
+        <Select v-model="filterModel.value" @change="filterCallback()" :options="statuses" placeholder="Select One" style="min-width: 12rem" :showClear="true" filterField="StatusType">
+                    <template #option="slotProps">
+                        <Tag :value="slotProps.option" >
+                            {{appStatusTypeLookup[slotProps.option]}}
+                        </Tag>
+                    </template>
+                    <template #value="slotProps">
+                        <div v-if="slotProps.value" class="flex items-center">
+                            {{appStatusTypeLookup[slotProps.value]}}
+                        </div>
+                        <div v-else>
+                            All
+                        </div>
+                    </template>
+                </Select>
+            </template>
         </Column>       
         <Column field="LastComplianceMessageDesc" header="LastComplianceMessageDesc"></Column>
         </DataTable>
